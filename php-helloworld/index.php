cee24950a084a8970f643cdcc925003acc4ce5f3
Josh Davis <jdavis@localhost.localdomain>
2021-02-20 09:19:07 -0600

    Added calc

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,31 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
+<?php
+
+/* Calculator */
+
+
+if($submit)
+{
+   if($operator == '*')
+   {
+       echo $numa * $numb;
+   } elseif($operator == '/')
+   {
+       echo $numa / $numb;
+   } elseif($operator == '+')
+   {
+       echo $numa + $numb;
+   } elseif($operator == '-')
+   {
+       echo $numa - $numb;
+   }
+} else { ?>
+<form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
+ <input type="text" name="numa" size="10">
+ <input type="text" name="operator" size="2">
+ <input type="text" name="numb" size="10">
+ <input type="submit" value="Calculate" name="submit">
+</form>
+<?php } ?> 
